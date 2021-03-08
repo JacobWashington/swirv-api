d3731681a265dfd2f0b5d39d225d2d540e8bf1e2
fletch8 <stampsfj04@gmail.com>
2021-03-08 17:19:04 -0500

    crud for storyline

--- controllers/storyline.js
+++ controllers/storyline.js
@@ -1,8 +1,60 @@
+const db = require('../models');
+
 // Controller
 const index = (req, res) => {
     res.json({ message: 'Storyline endpoint OK! ✅'});
 }
+const show = (req, res) => {
+    // Purpose: Fetch one storyline from DB and return
+    console.log('=====> Inside GET /storyline/:id');
+    console.log('=====> req.params');
+    console.log(req.params); // object used for finding storyline by id
+    db.Storyline.findById(req.params.id, (err, foundStoryline) => {
+        if (err) console.log('Error in storyline#show:', err);
+        res.json(foundStoryline);
+    });
+};
 
+const create = (req, res) => {
+    // Purpose: Create one storyline by adding body to DB, and return
+    console.log('=====> Inside POST /storyline');
+    console.log('=====> req.body');
+    console.log(req.body); // object used for creating new storyline
+    const userId = req.user.id
+    db.Storyline.create({authId: userId}, (err, savedStoryline) => {
+        if (err) console.log('Error in storyline#create:', err);
+        res.json(savedStoryline);
+    });
+};
+
+const update = (req, res) => {
+    // Purpose: Update one storyline in the DB, and return
+    console.log('=====> Inside PUT /storyline/:id');
+    console.log('=====> req.params');
+    console.log(req.params); // object used for finding Storyline by id
+    console.log('=====> req.body');
+    console.log(req.body); // object used for updating Storyline
+    db.Storyline.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, updatedStoryline) => {
+        if (err) console.log('Error in Storyline#update:', err);
+        res.json(updatedStoryline);
+    });
+};
+
+const destroy = (req, res) => {
+    // Purpose: Update one storyline in the DB, and return
+    console.log('=====> Inside DELETE /storyline/:id');
+    console.log('=====> req.params');
+    console.log(req.params); // object used for finding Storyline by id
+    db.Storyline.findByIdAndDelete(req.params.id, (err, deletedStoryline) => {
+        if (err) console.log('Error in storyline #destroy:', err);
+          res.sendStatus(200);
+          console.log(deletedStoryline);
+    });
+};
 module.exports = {
     index,
-}
+    show,
+    create,
+    update,
+    destroy,
+};
--- models/theGreatAttractor.js
+++ models/theGreatAttractor.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const theGreatAttractorSchema = new Schema({
-    id: Number,
     storylines: Array
 })
 
