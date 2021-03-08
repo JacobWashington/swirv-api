7451e713688cd09c89b9e665666d3ebd2875986c
fletch8 <stampsfj04@gmail.com>
2021-03-08 17:29:26 -0500

    added storyline crud routes

--- routes/storyline.js
+++ routes/storyline.js
@@ -2,7 +2,11 @@ const router = require('express').Router();
 const ctrl = require('../controllers');
 
 //routes
-router.get('/', ctrl.Storyline.index)
+router.get('/index', ctrl.Storyline.index)
+router.post('/', ctrl.Storyline.create)
+router.get("/:id", ctrl.Storyline.show);
+router.post("/:id", ctrl.Storyline.update);
+router.post("/:id", ctrl.Storyline.destroy)
 
 // exports
 module.exports = router;
