070abbea2677769637bc9ac880272ab72b82ea16
jacobwashington <jacobwashington.dev@gmail.com>
2021-03-09 16:11:11 -0800

    controllers: TGA ready for testing

--- controllers/theGreatAttractor.js
+++ controllers/theGreatAttractor.js
@@ -1,2 +1,34 @@
 // Database
 const db = require("../models");
+
+const consumeStoryline = async (req, res) => {
+  // if user makes offering to TGA,
+  // and offer == episode, create storyline where authId == TGA
+  // add episode to storyline.episodes array, set episode.authId == TGA
+  if (req.body.episodeId) {
+    const filter = { episodeId: req.body.episodeId };
+    const update = { authId: "the_great_attractor" };
+    let doc = await db.Storyline.findOneAndUpdate(filter, update, {
+      new: true,
+    });
+  } else if (req.body.storylineId) {
+    const filter = { storylineId: req.body.storylineId };
+    const update = { authId: "the_great_attractor" };
+    let doc = await db.Storyline.findOneAndUpdate(
+      filter,
+      update,
+      {
+        new: true,
+      },
+      (updatedStoryline) => {
+        for (let i = 0; i < updatedStoryline.episodes.length; i++) {
+          db.Episode.find({ episodeId: updatedStoryline.episodes[i] })
+            .then((foundEpisode) => {
+              foundEpisode.authId = "the_great_attractor";
+            })
+            .catch((error) => console.log(error));
+        }
+      }
+    );
+  }
+};
